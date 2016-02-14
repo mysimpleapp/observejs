8c59acabb9d252da0848a911de438f1c1e2be4ae
Nicolas CARREZ <nicolas.carrez@gmail.com>
2016-02-14 16:46:22 +0100

    Improve check objects

--- observe.js
+++ observe.js
@@ -1,25 +1,68 @@
 (function(){
-Observe = function(obj, arg1, arg2) {
-	var type = typeof arg1;
-	if(type=="function") Object.observe(obj, arg1);
-	else {
-		var check = null;
-		if(type=="string") check = checkString;
-		else if(type=="object") check = (arg1.length===undefined) ? checkObject : checkArray;
-		if(check) conditonalObserve(check, obj, arg1, arg2);
+Observe = function(obj, arg1, arg2, arg3) {
+	var type = typeof arg1
+	if(type=="function") {
+		next = arg1
+		doInitialExecute = observeWithoutInitialExecute(obj, null, arg1, arg2)
+	} else {
+		next = arg2
+		doInitialExecute = observeWithoutInitialExecute(obj, arg1, arg2, arg3)
 	}
-};
-var checkString = function(change, str) { return change.name===str; }
-var checkArray = function(change, arr) { return arr.indexOf(change.name)!==-1; }
-var checkObject = function(change, obj) { var name=change.name; return (name in obj) && obj[name]===change.object[name]; }
-var conditonalObserve = function(check, obj, val, callback) {
+	if(doInitialExecute) next()
+}
+var observeWithoutInitialExecute = function(obj, arg, next, doInitialExecute) {
+	if(!arg) {
+		Object.observe(obj, next)
+		doInitialExecute &= true
+	} else {
+		var type = typeof arg
+		if(type=="object" && arg.length!==undefined) {
+			var doInitialExecuteUnion = false
+			for(var i=0, len=arg.length; i<len; ++i)
+				doInitialExecuteUnion |= observeWithoutInitialExecute(obj, arg[i], next, doInitialExecute)
+			doInitialExecute &= doInitialExecuteUnion
+		} else {
+			var check = null, initialCheck = null
+			if(type=="string") { check = checkString; initialCheck = initialCheckString }
+			else if(type=="object") { check = checkObject; initialCheck = initialCheckObject }
+			if(check) {
+				doInitialExecute &= initialCheck(arg, obj)
+				conditonalObserve(check, obj, arg, next)
+			}
+		}
+	}
+	return doInitialExecute
+}
+var checkString = function(change, arg) {
+	return change.name===arg;
+}
+var initialCheckString = function(arg, obj) {
+	return (arg in obj)
+}
+var checkObject = function(change, arg) {
+	var name=change.name;
+	if(!(name in arg))
+		return false;
+	var obj = change.object
+	for(var a in arg)
+		if(arg[a]!==obj[a])
+			return false;
+	return true;
+}
+var initialCheckObject = function(arg, obj) {
+	for(var o in arg)
+		if(arg[o]!==obj[o])
+			return false
+	return true
+}
+var conditonalObserve = function(check, obj, val, next) {
 	Object.observe(obj, function(changes) {
 		for(var c=0, len=changes.length; c<len; ++c) {
 			if(check(changes[c], val)) {
-				callback();
+				next();
 				break;
 			}
 		}
-	});
+	})
 }
 })()
