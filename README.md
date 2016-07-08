be5b5f816faf830ad0424fdbda60290b63fcb9bc
Nicolas CARREZ <nicolas.carrez@gmail.com>
2016-07-08 08:10:25 +0200

    Replace Object.observe by Proxy

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # observejs
-Tiny javascript library that offer more developper friendly way to use Object.observe.
+Light javascript library based on javascript Proxy giving the possibility to create Observable objects in which it is possible to track any change.
+
+## Create Observable
+To ___create observables___, use the following syntax
+```javascript
+# The short way
+var obs = O({})
+
+# The full way
+var obs = Observable({})
+```
+
+You can directly include some properties
+```javascript
+var obs = O({a:1, b:2})
+```
+
+You can create observables containing observables
+```javascript
+var obs = O({a: O({b:1}) })
+```
+
+## Observe changes in observables
+To observe the change of an observable ___specific property___, use the following syntax:
+```javascript
+var obs = O({a:1})
+
+Observe(obs, "a", function(obs, attr, oldVal, newVal){
+	console.log("The property", attr, "of observable", obs, "was changed from", oldVal, "to", newVal)
+})
+
+obs.a = 2
+> "The property 'a' of observable 'obs' was changed from '1' to '2'"
+```
+
+You can observe the changes of ___any property___ of the observable, with this syntax:
+```javascript
+Observe(obs, "*", callback)
+```
+
+You can observe the changes of ___children specific property___ (providing there are also observables), with this sysntax:
+```javascript
+var obs = O({a: O({b: O({c:1}) }) })
+Observe(obs, "a.b.c", callback)
+```
+
+You can observe the changes of __any children property__ (providing children are also observables), with this sysntax:
+```javascript
+Observe(obs, "**", callback)
+```
+
+## Stop observation
+You can ___stop observation___, using this syntax:
+```javascript
+var observer = Observe(obs, "a", callback)
+observer.stop()
+```
--- observe.js
+++ observe.js
@@ -1,68 +1,150 @@
 (function(){
-Observe = function(obj, arg1, arg2, arg3) {
-	var type = typeof arg1
-	if(type=="function") {
-		next = arg1
-		doInitialExecute = observeWithoutInitialExecute(obj, null, arg1, arg2)
-	} else {
-		next = arg2
-		doInitialExecute = observeWithoutInitialExecute(obj, arg1, arg2, arg3)
-	}
-	if(doInitialExecute) next()
-}
-var observeWithoutInitialExecute = function(obj, arg, next, doInitialExecute) {
-	if(!arg) {
-		Object.observe(obj, next)
-		doInitialExecute &= true
-	} else {
-		var type = typeof arg
-		if(type=="object" && arg.length!==undefined) {
-			var doInitialExecuteUnion = false
-			for(var i=0, len=arg.length; i<len; ++i)
-				doInitialExecuteUnion |= observeWithoutInitialExecute(obj, arg[i], next, doInitialExecute)
-			doInitialExecute &= doInitialExecuteUnion
-		} else {
-			var check = null, initialCheck = null
-			if(type=="string") { check = checkString; initialCheck = initialCheckString }
-			else if(type=="object") { check = checkObject; initialCheck = initialCheckObject }
-			if(check) {
-				doInitialExecute &= initialCheck(arg, obj)
-				conditonalObserve(check, obj, arg, next)
-			}
-		}
-	}
-	return doInitialExecute
-}
-var checkString = function(change, arg) {
-	return change.name===arg;
-}
-var initialCheckString = function(arg, obj) {
-	return (arg in obj)
-}
-var checkObject = function(change, arg) {
-	var name=change.name;
-	if(!(name in arg))
-		return false;
-	var obj = change.object
-	for(var a in arg)
-		if(arg[a]!==obj[a])
-			return false;
-	return true;
-}
-var initialCheckObject = function(arg, obj) {
-	for(var o in arg)
-		if(arg[o]!==obj[o])
-			return false
-	return true
-}
-var conditonalObserve = function(check, obj, val, next) {
-	Object.observe(obj, function(changes) {
-		for(var c=0, len=changes.length; c<len; ++c) {
-			if(check(changes[c], val)) {
-				next();
-				break;
-			}
+// Create observable
+O = Observable = function(obj) {
+	var observers = {}
+	// create Observable as Proxy of given obj
+	var obs = new Proxy(obj, {
+		// Add trap for set
+		set: function(target, attr, val) {
+			var oldVal = target[attr]
+			// standard behaviour
+			target[attr] = val
+			// trigger change if value changed
+			if(oldVal!==val) triggerChange(obs, attr, oldVal, val);
+			return val
+		},
+		// add trap for delete
+		deleteProperty: function(target, attr) {
+			var oldVal = target[attr]
+			// standard behaviour
+			delete target[attr]
+			// trigger change if value existed
+			if(oldVal!==undefined) triggerChange(obs, attr, oldVal)
 		}
 	})
+	// Add _observers property
+	addHiddenProperty(obs, "_observers", observers)
+	return obs
+}
+// Observe observables
+Observe = function(obs, attrs, next) {
+	if(!isObservable(obs)) return
+	var observers = obs._observers
+	// check if it a deep change
+	var attrsParsed = parseAttrs(attrs), attr = attrsParsed[0], sonAttrs = attrsParsed[1]
+	if(attr==="**") sonAttrs="**"
+	if(!sonAttrs) {
+		// no deep change, create "observer" with given "next"
+		var obsObserver = new Observer(obs, attr, next)
+	} else {
+		// special observer for "**": trigger next at each change
+		if(attr==="**") {
+			var attr = "*"
+			var obsObserver = new Observer(obs, attr, function(changedObs, changedAttr, oldVal, newVal) {
+				// Add observation to new son
+				this.setSonObserver(changedAttr, Observe(newVal, sonAttrs, next))
+				// call next
+				next(changedObs, changedAttr, oldVal, newVal)
+			})
+		// otherwise, trigger next only if targeted deep attr has changed
+		} else {
+			var obsObserver = new Observer(obs, attr, function(changedObs, changedAttr, oldVal, newVal) {
+				// Add observation to new son
+				this.setSonObserver(changedAttr, Observe(newVal, sonAttrs, next))
+				// check if the deep value has changed, if so call "next"
+				var oldDeepVal = getDeepVal(oldVal, sonAttrs)
+				var newDeepVal = getDeepVal(newVal, sonAttrs)
+				if(oldDeepVal!==newDeepVal) next(changedObs, changedAttr, oldVal, newVal)
+			})
+		}
+		// Add observation to son(s)
+		if(attr!=="*")
+			obsObserver.setSonObserver(attr, Observe(obs[attr], sonAttrs, next))
+		else
+			for(var sAttr in obs)
+				obsObserver.setSonObserver(sAttr, Observe(obs[sAttr], sonAttrs, next))
+	}
+	// Fill "_observers"
+	var attrObservers = observers[attr]
+	if(!attrObservers) attrObservers = observers[attr] = []
+	attrObservers.push(obsObserver)
+	return obsObserver
+}
+// Observer object
+var Observer = function(obs, attr, next) {
+	this.obs = obs
+	this.attr = attr
+	this.next = next
+	// stop observation
+	this.stop = function(){
+		var obs = this.obs
+		if(!isObservable(obs)) return
+		// find this in _observers
+		var observers = obs._observers
+		var attrObservers = observers[this.attr]
+		if(!attrObservers) return
+		var index = attrObservers.indexOf(this)
+		// if found, remove it from _observers
+		if(index!==-1) attrObservers.splice(index)
+		// if there is sonObservers, stop them all
+		var sonObservers = this.sonObservers
+		if(sonObservers)
+			for(var sonAttr in sonObservers)
+				sonObservers[sonAttr].stop()
+	}
+	this.setSonObserver = function(attr, sonObserver) {
+		if(!sonObserver) return
+		// create sonObservers map (if needed)
+		var sonObservers = this.sonObservers
+		if(!sonObservers) sonObservers = this.sonObservers = {}
+		// stop previous sonObserver on same attr (if any)
+		var oldSonObserver = sonObservers[attr]
+		if(oldSonObserver) oldSonObserver.stop()
+		// fill sonObservers
+		sonObservers[attr] = sonObserver
+	}
+}
+// Add property that is not visible by a "for in"
+var addHiddenProperty = function(obj, attr, method) {
+	Object.defineProperty(obj, attr, {
+		enumerable: false,
+		configurable: true,
+		writable: true,
+		value: method
+	})
+}
+// Trigger change information
+var triggerChange = function(obs, attr, oldVal, newVal) {
+	var observers = obs._observers
+	// Specigic attr observers
+	callObservers(observers[attr], obs, attr, oldVal, newVal)
+	// All attrs observers
+	callObservers(observers['*'], obs, attr, oldVal, newVal)
+}
+var callObservers = function(observers, obs, attr, oldVal, newVal){
+	if(!observers) return
+	for(var i=0, len=observers.length; i<len; ++i)
+		observers[i].next(obs, attr, oldVal, newVal)
+}
+// Parse "a.a.a" into ["a","a.a"]
+var parseAttrs = function(attrs) {
+	var firstPoint = attrs.indexOf(".")
+	if(firstPoint===-1) return [attrs]
+	else return [attrs.substr(0, firstPoint), attrs.substr(firstPoint+1)]
+}
+// get value of obj["a.a"]
+var getDeepVal = function(obj, attrs) {
+	if(!isObject(obj)) return undefined
+	var attrsParsed = parseAttrs(attrs)
+	if(!attrsParsed[1]) return obj[attrs]
+	else return getDeepVal(obj[attrsParsed[0]], attrsParsed[1])
+}
+// Check if variable is an Object
+var isObject = function(obs) {
+	return (obs && typeof obs==="object")
+}
+// Check if variable is an Observable
+var isObservable = function(obs) {
+	return (isObject(obs) && obs._observers!==undefined)
 }
 })()
